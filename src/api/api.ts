d5eaaf98da6f61a27e7c7d452eb65e4e036c1ed5
ivan <curkanivan493@gmail.com>
2021-11-04 02:17:36 +0200

    hash

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,13 @@
 import axios, { AxiosResponse } from "axios";
 import { IUser } from "../models/IUser";
 
+const instance = axios.create({
+    
+    withCredentials: false
+})
+
 export default class Api {
     static async getUsers(): Promise<AxiosResponse<IUser[]>>{
-        return axios.get<IUser[]>('./users.json')
+        return instance.get<IUser[]>('https://gist.githubusercontent.com/ivannwebdev/adbc10764b5bb3b18b0493d2bd84962a/raw/1b5494b79894f5984a1a25156e3fa0ba127afb7f/users.json')
     }
 }
